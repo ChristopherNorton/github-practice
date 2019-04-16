b8ba3d75a2d08a4372f4bd0f027127db97048bb8
ChristopherNorton <chrisdnorton@gmail.com>
2019-04-16 16:12:15 -0700

    updated rule-out function and added tests

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,9 +1,26 @@
 // Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
 
-function oddOrEven(){
-    console.log('result')
+function trueNumber (number) {
+  if(isNaN(number) === false){
+  return oddOrEven(number)
+} else {
+  console.log('Invalid input')
+  }
 }
 
-oddOrEven(4)
-oddOrEven(11)
-oddOrEven(75)
+function oddOrEven(number){
+  if(number === 0) {
+    console.log('Zero');
+  }else if (number % 2 === 0){
+    console.log('Even number')
+  } else if (number % 2 !=0) {
+    console.log('Odd number');
+  } else {
+    console.log('Invalid input')
+  }
+}
+
+trueNumber(10)
+trueNumber(0)
+trueNumber('fi')
+trueNumber(-2384)
